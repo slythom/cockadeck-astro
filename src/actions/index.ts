5cd37fd54df280d1453074840d22d7860e8cac9a
slythom <contact@sylvainthomas.fr>
2025-09-09 22:24:54 +0200

    Added: Action to fetch card data (Scryfall API)

---
+++ src/actions/index.ts
@@ -0,0 +1,19 @@
+import { defineAction } from 'astro:actions';
+import { z } from 'astro:schema';
+
+export const server = {
+    searchCard: defineAction({
+        accept: 'form',
+        input: z.object({
+            quantity: z.coerce.number(),
+            setcode: z.string(),
+            collectornumber: z.string(),
+        }),
+        handler: async ({ quantity, setcode, collectornumber }) => { 
+            const response = await fetch(`https://api.scryfall.com/cards/${setcode}/${collectornumber}`);
+            const cardFound = await response.json();
+            console.log(cardFound.image_uris.large)
+            // return cardFound.image_uris.large
+            },
+    })
+}
